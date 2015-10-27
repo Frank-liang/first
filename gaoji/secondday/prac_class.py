a04121ac7d7707a9fff4db2d019850096763b919
root <root@ldap1.zcool.com.cn>
2015-10-27 16:46:25 +0800

    管理机网其他机器打key的脚本

---
+++ gaoji/secondday/prac_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+class unit:
+    def __init__(self):
+    self._blood_max = self._blood
+    print self.name + "created"
+
+class attacker(unit):
+    def attack(self,u):
+        u._blood <=0:
+        print u
+
+
+
