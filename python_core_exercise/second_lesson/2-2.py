e9d15c758e084d153e49e9c2d6b92e68c09ad29e
root <root@ldap1.zcool.com.cn>
2015-11-06 18:38:46 +0800

    Python core programe

---
+++ python_core_exercise/second_lesson/2-2.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+#coding=utf-8
+ends = 1 + 2*4
+print "The result is %s" %ends 
---
+++ python_core_exercise/second_lesson/2-6.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+Number = int(raw_input("Input a number: "))
+if Number < 0:
+    print '%d is nagative' %Number
+elif Number == 0:
+    print '%d  == 0' %Number
+else:
+    print '%d is positive' %Number
